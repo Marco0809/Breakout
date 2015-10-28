02d942b688e74f7e1552deb5b9ffa812435aaf6a
MbL93 <Marcblickle@gmx.de>
2015-10-28 12:12:37 +0100

    Menü mit Startbug

--- source/mainmenu.js
+++ source/mainmenu.js
@@ -1,8 +1,18 @@
 Break.MainMenu = function (game) {
-    var button1, button2, button3, button4,supermario, boden, button1count, button2count, button3count,button4count;
-    
+    var button1, button2, button3, button4, button5, supermario, boden;
+   
 };
-
+    var button1count=0;
+    var button2count=0; 
+    var button3count=0;
+    var button4count=0; 
+    var button5count=0;
+    var startstate = 0;
+    var soundstate = 0;
+    var editorstate=0;
+    var soundon = true;
+    var musicon = true;
+    
 Break.MainMenu.prototype = {
     create: function () {
         this.physics.startSystem(Phaser.Physics.ARCADE);
@@ -12,7 +22,7 @@ Break.MainMenu.prototype = {
         background = this.add.tileSprite(0, 0, 1024, 768, 'mario0');
 
         //Mario
-        supermario = this.add.sprite(190, 580, 'supermario');
+        supermario = this.add.sprite(190, 635, 'supermario');
         supermario.scale.set(0.5);
         supermario.anchor.set(0.5);
         this.physics.arcade.enable(supermario);
@@ -74,30 +84,35 @@ Break.MainMenu.prototype = {
         button4.checkWorldBounds = true;
         button4.body.bounce.set(1);
         
-        //Startbeschriftung
-        startfont= this.add.sprite(110, 395, 'startfont');
-        soundfont= this.add.sprite(326, 395, 'soundfont');
-        weicheifont= this.add.sprite(542, 397, 'weicheifont');
-        editorfont= this.add.sprite(758, 395, 'editorfont');
-        startfont.scale.set(0.13);
-        soundfont.scale.set(0.13);
-        weicheifont.scale.set(0.13);
-        editorfont.scale.set(0.13);
         
-        button1count = 0;
-        button2count = 0;
-        button3count = 0;
-        button4count = 0;
-        
-        
-        // Deklarieren der übrigen BEschriftungen
-        heldfont= this.add.sprite(564, 398, 'heldfont');
-        heldfont.scale.set(0.13);
-        heldfont.visible=false;
+        //Ganz Rechter Knopf für Levelmenü
 
-        psychofont= this.add.sprite(543, 399, 'psychofont');
-        psychofont.scale.set(0.13);
-        psychofont.visible=false;
+        button5= this.add.sprite(860, 422, 'menuebutton');
+        button5.anchor.setTo(0.5,0.5);
+        button5.scale.set(0.13);
+        this.physics.arcade.enable(button5);
+        button5.body.immovable = true;
+        button5.body.collideWorldBounds = true;
+        button5.checkWorldBounds = true;
+        button5.body.bounce.set(1);
+        button5.body.enable=false;
+        button5.visible=false;
+        
+        //Startbeschriftung
+        startlabel= this.add.sprite(110, 395, 'startlabel');
+        soundlabel= this.add.sprite(326, 395, 'soundlabel');
+        weicheilabel= this.add.sprite(542, 398, 'weicheilabel');
+        editorlabel= this.add.sprite(758, 395, 'editorlabel');
+        startlabel.scale.set(0.13);
+        soundlabel.scale.set(0.13);
+        weicheilabel.scale.set(0.13);
+        editorlabel.scale.set(0.13);
+        
+        
+        
+        
+        this.buildLabels();
+        
         
 
       
@@ -112,6 +127,7 @@ Break.MainMenu.prototype = {
         this.physics.arcade.collide(supermario, button2, this.marioHitButton2, null, this);
         this.physics.arcade.collide(supermario, button3, this.marioHitButton3, null, this);
         this.physics.arcade.collide(supermario, button4, this.marioHitButton4, null, this);
+        this.physics.arcade.collide(supermario, button5, this.marioHitButton5, null, this);
         
 	
 
@@ -150,15 +166,113 @@ Break.MainMenu.prototype = {
 	
 	},
 	
+    buildLabels: function() {
+        
+        
+        // Deklarieren der übrigen, zunächst unsichtbaren, Beschriftungen
+        heldlabel= this.add.sprite(564, 398, 'heldlabel');
+        heldlabel.scale.set(0.13);
+        heldlabel.visible=false;
 
+        psycholabel= this.add.sprite(543, 399, 'psycholabel');
+        psycholabel.scale.set(0.13);
+        psycholabel.visible=false;
+        
+        multilabel= this.add.sprite(326, 395, 'multilabel');
+        multilabel.scale.set(0.13);
+        multilabel.visible=false;
+
+        botlabel= this.add.sprite(542, 397, 'botlabel');
+        botlabel.scale.set(0.13);
+        botlabel.visible=false;
+        
+        singlelabel= this.add.sprite(110, 395, 'singlelabel');
+        singlelabel.scale.set(0.13);
+        singlelabel.visible=false;
+
+        zuruecklabel= this.add.sprite(758, 397, 'zuruecklabel');
+        zuruecklabel.scale.set(0.13);
+        zuruecklabel.visible=false;
+        
+        
+        level1label= this.add.sprite(81, 397, 'level1label');
+        level1label.scale.set(0.13);
+        level1label.visible=false;
+
+        level2label= this.add.sprite(256, 397, 'level2label');
+        level2label.scale.set(0.13);
+        level2label.visible=false;
+        
+        level3label= this.add.sprite(431, 397, 'level3label');
+        level3label.scale.set(0.13);
+        level3label.visible=false;
+
+        eigenelabel= this.add.sprite(606, 397, 'eigenelabel');
+        eigenelabel.scale.set(0.13);
+        eigenelabel.visible=false;
+        
+        soundonlabel= this.add.sprite(276, 389, 'soundonlabel');
+        soundonlabel.scale.set(0.13);
+        soundonlabel.visible=false;
+
+        soundofflabel= this.add.sprite(276, 389, 'soundofflabel');
+        soundofflabel.scale.set(0.13);
+        soundofflabel.visible=false;
+        
+        musiconlabel= this.add.sprite(470, 391, 'musiconlabel');
+        musiconlabel.scale.set(0.13);
+        musiconlabel.visible=false;
+
+        musicofflabel= this.add.sprite(470, 391, 'musicofflabel');
+        musicofflabel.scale.set(0.13);
+        musicofflabel.visible=false;
+        
+    
+    },
     
     marioHitButton1: function() {
-       
         if(supermario.body.touching.up){
+            if(soundstate==0 && editorstate==0 &&startstate==0){ 
+                startstate =1;
+                switch(button1count) {
+                    case 0:
+                        
+                        this.showPlayerMenu();
+                         button1count+=1;
+                        break;
+                    case 1:
+                        this.showLevelMenu();
+                        button1count+=1;
+                        startstate=2;
+                        break;
+                    case 2:
+                        this.startLevel1();
+                        button1count=0;
+                        startstate=0;
+                        break;
+
+                }
+            }
+            
+            if(soundstate==1 ){
+                switch(soundon) {
+                    case true:
+                       
+                        soundonlabel.visible=false;
+                        soundofflabel.visible=true;
+                        soundon=false;
+                        break;
+                    case false:
+                        soundonlabel.visible=true;
+                        soundofflabel.visible=false;
+                        soundon=true;
+                        break;
+                    
+
+                }
            
-       
-            
-            
+           
+            }
         }
     },
     
@@ -166,31 +280,135 @@ Break.MainMenu.prototype = {
         
         if(supermario.body.touching.up){
             
+            if(soundstate==1){
+                switch(musicon) {
+                    case false:
+                        musiconlabel.visible=true;
+                        musicofflabel.visible=false;
+                        musicon=true;
+                        break;
+                    case true:
+                        musiconlabel.visible=false;
+                        musicofflabel.visible=true;
+                        musicon=false;
+
+
+                        break;
+                    
+
+                }
+            }
             
-           
+            
+            if(startstate==0 && editorstate==0 && soundstate==0){
+                        if(soundon){
+                            soundonlabel.visible=true;
+                        } 
+                        else {
+                            soundofflabel.visible=true;
+                        }
+                
+                        if(musicon){
+                           musiconlabel.visible=true;
+                        }
+                    	else{
+                            musicofflabel.visible=true;
+                          
+                        }
+                
+                        
+                        startlabel.visible=false;
+                        soundlabel.visible=false;
+                        weicheilabel.visible=false;
+                        heldlabel.visible=false;
+                        psycholabel.visible=false;
+                        editorlabel.visible=false;
+
+                        
+                        zuruecklabel.x=640
+                        zuruecklabel.visible=true;
+
+                        button1.x = 310;
+                        button2.x = 510;
+                        button3.x = 710;
+                        button4.body.enable=false;
+                        button4.visible=false;
+                
+                        soundstate=1;
+                
+            }
+            
+            
+                       
+            if(startstate==1){
+               this.showLevelMenu(); 
+            }
+            
+            if(startstate==2){
+               //Start Level2 
+            }
+            
+            
+            
+                
         }
     },
     
     marioHitButton3: function() {
         if(supermario.body.touching.up){
-        switch(button3count) {
-            case 0:
-                weicheifont.visible=false;
-                heldfont.visible=true;
-                button3count+=1;
-                break;
-            case 1:
-                heldfont.visible=false;
-                psychofont.visible=true;
-                button3count+=1;
-                break;
-            case 2:
-                psychofont.visible=false;
-                weicheifont.visible=true;
-                button3count=0;
-                break;
+            if(startstate==0 && editorstate==0 && soundstate==0){
+                switch(button3count) {
+                    case 0:
+                        weicheilabel.visible=false;
+                        heldlabel.visible=true;
+                        button3count+=1;
+                        break;
+                    case 1:
+                        heldlabel.visible=false;
+                        psycholabel.visible=true;
+                        button3count+=1;
+                        break;
+                    case 2:
+                        psycholabel.visible=false;
+                        weicheilabel.visible=true;
+                        button3count=0;
+                        break;
 
-        }
+                }
+            }
+            
+            if(startstate==1) {
+                this.showLevelMenu();
+            }
+            
+            if(soundstate==1) {
+                soundstate=0;
+                startlabel.visible=true;
+                soundlabel.visible=true;
+                weicheilabel.visible=true;
+                editorlabel.visible=true;  
+                
+                
+
+                musicofflabel.visible=false;
+                musiconlabel.visible=false;
+                soundofflabel.visible=false;
+                soundonlabel.visible=false;
+                
+                zuruecklabel.x=758
+                zuruecklabel.visible=false;
+
+                button1.x =180;
+                button2.x = 396;
+                button3.x = 612;
+                button4.body.enable=true;
+                button4.visible=true;
+                
+                
+            }
+            
+            
+            
                 
                 
         
@@ -201,11 +419,61 @@ Break.MainMenu.prototype = {
     
     marioHitButton4: function() {
         if(supermario.body.touching.up){
-           // this.game.state.start('Game');
+           
+            this.startLevel1();
+        }
+        
+        
+    },
+    
+    marioHitButton5: function() {
+        if(supermario.body.touching.up){
             
         }
         
         
+    },
+    
+    showLevelMenu: function(){
+        
+        singlelabel.visible=false;
+        multilabel.visible=false;
+        botlabel.visible=false;
+        zuruecklabel.x=false;
+
+        button1.x = 150;
+        button2.x= 325;
+        button3.x=500;
+        button4.x=675;
+        button5.body.enable=true;
+        button5.visible=true;
+        zuruecklabel.x=790;
+
+        level1label.visible=true;
+        level2label.visible=true;
+        level3label.visible=true;
+        eigenelabel.visible=true;
+        
+    },
+    
+    showPlayerMenu: function(){
+        startlabel.visible=false;
+        soundlabel.visible=false;
+        weicheilabel.visible=false;
+        heldlabel.visible=false;
+        psycholabel.visible=false;
+        editorlabel.visible=false;
+
+        singlelabel.visible=true;
+        multilabel.visible=true;
+        botlabel.visible=true;
+        zuruecklabel.visible=true;
+       
+        startstate=1;
+    },
+    
+    startLevel1: function() {
+        this.game.state.start('Game');
     }
     
    
--- source/preload.js
+++ source/preload.js
@@ -7,21 +7,45 @@ Break.Preloader.prototype = {
         
         
     this.load.image('cursor', 'images/cursor.png');
+    this.load.image('ball', 'images/ball.png');
+        
+        //Bloecke
     this.load.image('leicht', 'images/bloecke/blockleicht/leicht.png');
     this.load.image('Schwer1', 'images/bloecke/blockschwer/StufeEins.png');
     this.load.image('Schwer2', 'images/bloecke/blockschwer/StufeZwei.png');
     this.load.image('Schwer3', 'images/bloecke/blockschwer/StufeDrei.png');
     this.load.image('Schwer4', 'images/bloecke/blockschwer/StufeVier.png');
-    this.load.image('ball', 'images/ball.png');
-    this.load.image('mario1','images/hintergrund.png');
-    this.load.image('mario0','images/startbackground.png');
-    this.load.image('startfont','images/startfont.png'); 
-    this.load.image('soundfont','images/soundfont.png');
-        this.load.image('editorfont','images/editorfont.png');
-    this.load.image('weicheifont','images/weicheifont.png');
-        this.load.image('heldfont','images/heldfont.png');
-        this.load.image('psychofont','images/psychofont.png');  
-    this.load.image('supermario','images/supermario.png');  
+    
+        
+        //Hintergruende
+    this.load.image('mario1','images/hintergruende/hintergrund.png');
+    this.load.image('mario0','images/hintergruende/startbackground.png');
+    this.load.image('startlabel','images/labels/startlabel.png'); 
+    this.load.image('soundlabel','images/labels/soundlabel.png');
+        
+        //Menü-Labels
+    this.load.image('editorlabel','images/labels/editorlabel.png');
+    this.load.image('weicheilabel','images/labels/weicheilabel.png');
+    this.load.image('heldlabel','images/labels/heldlabel.png');
+    this.load.image('psycholabel','images/labels/psycholabel.png');
+        
+    this.load.image('multilabel','images/labels/multilabel.png');
+    this.load.image('botlabel','images/labels/botlabel.png');
+    this.load.image('singlelabel','images/labels/singlelabel.png');
+    this.load.image('zuruecklabel','images/labels/zuruecklabel.png');
+        
+    this.load.image('level1label','images/labels/level1label.png');
+    this.load.image('level2label','images/labels/level2label.png');
+    this.load.image('level3label','images/labels/level3label.png');
+    this.load.image('eigenelabel','images/labels/eigenelabel.png');
+        
+    this.load.image('musicofflabel','images/labels/musicofflabel.png');
+    this.load.image('musiconlabel','images/labels/musiconlabel.png');
+    this.load.image('soundofflabel','images/labels/soundofflabel.png');
+    this.load.image('soundonlabel','images/labels/soundonlabel.png'); 
+        
+        //Menü
+    this.load.image('supermario','images/supermario.png');
     this.load.image('boden','images/boden.png'); 
     this.load.image('test1','images/test1.png');  
     this.load.image('test2','images/test2.png');
